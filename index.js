48f181bf06cfa2088659734068ddf43cbea64b23
Kazuya-Pum <kazuya.pum@gmail.com>
2019-06-19 13:55:00 +0900

    webhook

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+const server = http.createServer(function (request, response) {
+    if ((request.url === '/webhook/') && (request.method === 'POST')) {
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        response.end("POST request is detected");
+    }
 });
 
 const port = process.env.PORT || 1337;
