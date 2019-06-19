f1cc7171b54b5b1604a6b9ef29e66ce841f2ab48
Kazuya-Pum <kazuya.pum@gmail.com>
2019-06-19 14:17:07 +0900

    sample

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-const http = require('http');
+'use strict';
 
-const server = http.createServer(function (request, response) {
-    if ((request.url === '/webhook/') && (request.method === 'POST')) {
-        response.writeHead(200, { "Content-Type": "text/plain" });
-        response.end("POST request is detected");
-    }
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+// create LINE SDK config from env variables
+const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/webhook', line.middleware(config), (req, res) => {
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result))
+        .catch((err) => {
+            console.error(err);
+            res.status(500).end();
+        });
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
+// event handler
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        // ignore non-text-message event
+        return Promise.resolve(null);
+    }
 
-console.log("Server running at http://localhost:%d", port);
+    // create a echoing text message
+    const echo = { type: 'text', text: event.message.text };
+
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
