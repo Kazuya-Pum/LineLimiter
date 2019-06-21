fcc3daee29ac2e287e72d03e9f1390f47233089b
Kazuya-Pum <kazuya.pum@gmail.com>
2019-06-21 14:32:10 +0900

    エラー修正

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const debug = require('debug');
 const line = require('@line/bot-sdk');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const logger = require('morgan');
 
 const webhook = require('./routes/webhook');
 const add = require('./routes/add');
@@ -17,6 +19,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -36,4 +39,8 @@ app.use(function (req, res, next) {
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`listening on ${port}`);
+});
+
+const server = app.listen(app.get('port'), function () {
+    debug('Express server listening on port ' + server.address().port);
 });
